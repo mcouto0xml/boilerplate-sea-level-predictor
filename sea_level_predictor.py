3595159b093aeef78e19e8092db9ad556f47da9e
mcouto0xml <murilo.oliveira@sou.inteli.edu.br>
2024-09-26 20:57:07 -0300

    feat: adiciona exercicio

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -3,21 +3,30 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
 def draw_plot():
-    # Read data from file
+    # Ler os dados do arquivo
+    df = pd.read_csv('epa-sea-level.csv')
 
+    # Criar o gráfico de dispersão
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
 
-    # Create scatter plot
+    # Criar a primeira linha de tendência (usando todos os dados)
+    slope, intercept, r_value, p_value, std_err = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    years_extended = pd.Series(range(1880, 2051))
+    plt.plot(years_extended, intercept + slope * years_extended, color='red')
 
-
-    # Create first line of best fit
-
-
-    # Create second line of best fit
-
-
-    # Add labels and title
-
+    # Criar a segunda linha de tendência (usando dados a partir de 2000)
+    recent_df = df[df['Year'] >= 2000]
+    slope_recent, intercept_recent, r_value_recent, p_value_recent, std_err_recent = linregress(recent_df['Year'], recent_df['CSIRO Adjusted Sea Level'])
     
-    # Save plot and return data for testing (DO NOT MODIFY)
+    # Plotar a segunda linha de tendência no mesmo intervalo de anos (2000-2050)
+    years_recent = pd.Series(range(2000, 2051))
+    plt.plot(years_recent, intercept_recent + slope_recent * years_recent, color='green')
+
+    # Adicionar os rótulos e o título
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+
+    # Salvar o gráfico e retornar
     plt.savefig('sea_level_plot.png')
     return plt.gca()
